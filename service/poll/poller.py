4b6bfaa2c06b048aad69dd0965c78558979b5fb5
Matthew <mcgarr@alumni.unc.edu>
2023-03-06 19:22:54 -0500

    Added Service Models

--- service/poll/poller.py
+++ service/poll/poller.py
@@ -11,12 +11,23 @@ django.setup()
 
 # Import models from service_rest, here.
 # from service_rest.models import Something
+from service_rest.models import AutomobileVO
+
+def get_automobiles():
+    response = requests.get("http://inventory-api:8000/api/automobiles/")
+    content = json.loads(response.content)
+    for automobile in content["automobile"]:
+        AutomobileVO.update_or_create(
+            import_href=bin["href"],
+            defaults={}
+        )
 
 def poll():
     while True:
         print('Service poller polling for data')
         try:
             # Write your polling logic, here
+            get_automobiles()
             pass
         except Exception as e:
             print(e, file=sys.stderr)
